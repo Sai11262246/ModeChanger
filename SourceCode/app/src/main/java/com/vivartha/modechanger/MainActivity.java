9426c05e080aa86021673394cbac1125c46a6250
vikasvch <43013418+vikasvch@users.noreply.github.com>
2018-10-29 22:16:40 -0500

    Functional tetsing code changes checing in
    Functional tetsing code changes checing in

--- SourceCode/app/src/main/java/com/vivartha/modechanger/MainActivity.java
+++ SourceCode/app/src/main/java/com/vivartha/modechanger/MainActivity.java
@@ -1,6 +1,7 @@
 package com.vivartha.modechanger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,9 +15,9 @@ public class MainActivity extends Activity {
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
     private final String DEFAULT="";
-    EditText r,v,s,vu;
+    EditText r,v,s;
     Button save;
-    String ring,vibrate,silent,volumeup;
+    String ring,vibrate,silent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,15 +26,43 @@ public class MainActivity extends Activity {
         r=(EditText)findViewById(R.id.editText1);
         v=(EditText)findViewById(R.id.editText2);
         s=(EditText)findViewById(R.id.editText3);
-        vu=(EditText)findViewById(R.id.editText4);
+        //vu=(EditText)findViewById(R.id.editText4);
         save = (Button)findViewById(R.id.button1);
         //check for the shared preferences;
         preferences = getSharedPreferences("modes", MODE_PRIVATE);
         ring = preferences.getString("ring_key", DEFAULT);
         vibrate = preferences.getString("vibrate_key", DEFAULT);
         silent = preferences.getString("silent_key", DEFAULT);
-        volumeup = preferences.getString("volume_up", DEFAULT);
-        if(ring.equals(DEFAULT)||vibrate.equals(DEFAULT)||silent.equals(DEFAULT)||volumeup.equals(DEFAULT)){
+        //volumeup = preferences.getString("volume_up", DEFAULT);
+
+        //This will set the keyword for RINGER MODE as ring if it is not configured by user
+        if(ring.equals(DEFAULT))
+        {
+            editor = preferences.edit();
+            editor.putString("ring_key", "ring");
+            editor.commit();
+            ring = preferences.getString("ring_key", DEFAULT);
+        }
+
+        //This will set the keyword for VIBRATE MODE as vibrate if it is not configured by user
+        if(vibrate.equals(DEFAULT))
+        {
+            editor = preferences.edit();
+            editor.putString("vibrate_key", "vibrate");
+            editor.commit();
+            vibrate = preferences.getString("vibrate_key", DEFAULT);
+        }
+
+        //This will set the keyword for SILENT MODE as silent if it is not configured by user
+        if(silent.equals(DEFAULT))
+        {
+            editor = preferences.edit();
+            editor.putString("silent_key", "silent");
+            editor.commit();
+            silent = preferences.getString("silent_key", DEFAULT);
+        }
+
+        /*if(ring.equals(DEFAULT)||vibrate.equals(DEFAULT)||silent.equals(DEFAULT)||volumeup.equals(DEFAULT)){
             editor = preferences.edit();
             editor.putString("ring_key", "ring");
             editor.putString("vibrate_key", "vibrate");
@@ -44,11 +73,13 @@ public class MainActivity extends Activity {
             vibrate = preferences.getString("vibrate_key", DEFAULT);
             silent = preferences.getString("silent_key", DEFAULT);
             volumeup = preferences.getString("volumeup_key", DEFAULT);
-        }
+        }*/
+
+        //Setting keyword values to GUI layout
         r.setText(ring);
         v.setText(vibrate);
         s.setText(silent);
-        vu.setText(volumeup);
+        //vu.setText(volumeup);
         save.setOnClickListener(new OnClickListener() {
 
             @Override
@@ -56,14 +87,24 @@ public class MainActivity extends Activity {
                 String temp_ring = r.getText().toString().trim();
                 String temp_vibrate = v.getText().toString().trim();
                 String temp_silent = s.getText().toString().trim();
-                String temp_volumeup = vu.getText().toString().trim();
+                //String temp_volumeup = vu.getText().toString().trim();
                 editor = preferences.edit();
                 editor.putString("ring_key", temp_ring);
                 editor.putString("vibrate_key", temp_vibrate);
                 editor.putString("silent_key", temp_silent);
-                editor.putString("volumeup_key", temp_volumeup);
+                //editor.putString("volumeup_key", temp_volumeup);
                 editor.commit();
                 Toast.makeText(getApplicationContext(), "SAVED!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Action to GoBack from edit screen to home screen
+        Button btn = (Button) findViewById(R.id.btn_goback);
+        btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Home_Activity.class);
+                startActivity(i);
             }
         });
     }
--- SourceCode/app/src/main/java/com/vivartha/modechanger/about_us.java
+++ SourceCode/app/src/main/java/com/vivartha/modechanger/about_us.java
@@ -1,7 +1,10 @@
 package com.vivartha.modechanger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
 public class about_us extends Activity {
 
@@ -9,6 +12,15 @@ public class about_us extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        Button btn = (Button)findViewById(R.id.r5);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(about_us.this, Home_Activity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
