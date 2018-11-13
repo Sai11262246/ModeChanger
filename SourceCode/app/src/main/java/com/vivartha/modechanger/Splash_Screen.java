a6106220a4cc3ecaa3e3b6cdec4e893c59a77c32
saitejareddymalle <42991623+saitejareddymalle@users.noreply.github.com>
2018-11-12 21:24:23 -0600

    User registration code checkin
    User registration code checkin

--- SourceCode/app/src/main/java/com/vivartha/modechanger/Splash_Screen.java
+++ SourceCode/app/src/main/java/com/vivartha/modechanger/Splash_Screen.java
@@ -1,9 +1,13 @@
 package com.vivartha.modechanger;
 
-        import android.content.Intent;
+import android.content.Intent;
         import android.os.Bundle;
         import android.app.Activity;
 
+/**
+ * @author sai krsihna
+ * This is a splash screen activity
+  */
 public class Splash_Screen extends Activity {
 
     @Override
@@ -11,10 +15,36 @@ public class Splash_Screen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
 
-        Intent intent = new Intent(getApplicationContext(),
-                LoginActivity.class);
-        startActivity(intent);
-        finish();
+        final AppPreferences mAppPreferences = new AppPreferences(this);
+
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                    if(mAppPreferences.getLoginState() == 0){
+                        Intent intent = new Intent(getApplicationContext(),   LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else if(mAppPreferences.getLoginState() == 1){
+                        Intent intent = new Intent(getApplicationContext(),   PinPadActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+
+
+                }
+            }
+        };
+        timer.start();
+
+
+
     }
 
 }
