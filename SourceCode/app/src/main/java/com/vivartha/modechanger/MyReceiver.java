171688e3458f9904a1287f57540a360cae8f4472
SiriGogineni <42917241+SiriGogineni@users.noreply.github.com>
2018-11-12 21:26:08 -0600

    User login activity cde checked in
    User login activity cde checked in

--- SourceCode/app/src/main/java/com/vivartha/modechanger/MyReceiver.java
+++ SourceCode/app/src/main/java/com/vivartha/modechanger/MyReceiver.java
@@ -1,19 +1,36 @@
 package com.vivartha.modechanger;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.SmsManager;
+import android.util.Log;
 import android.widget.Toast;
+
+/**
+ * @author siri gogineni
+ * This runs as an service in the background.
+ * Retrieves the received message and checks whether it is matched.
+ * If matched, performs specified acion
+ */
 
 public class MyReceiver extends BroadcastReceiver {
     AudioManager am;
     SharedPreferences preferences;
-    String ring,vibrate,silent,voluming;
-    private final String DEFAULT="";
+    String ring, vibrate, silent, voluming;
+    private final String DEFAULT = "";
+
+    @SuppressWarnings("deprecation")
     @Override
     public void onReceive(Context context, Intent intent) {
         final Bundle bundle = intent.getExtras();
@@ -36,8 +53,12 @@ public class MyReceiver extends BroadcastReceiver {
                     SmsMessage currentMessage = SmsMessage
                             .createFromPdu((byte[]) pdusObj[i]);
                     String actual_message = currentMessage.getDisplayMessageBody();
+                    String sender = currentMessage.getOriginatingAddress();
+
+                    Log.e("Receiver", "sender : "+ sender);
+
                     String message = getFirstWord(actual_message);
-                    int status = changeMode(message);
+                    int status = changeMode(message, actual_message, context);
                     switch (status) {
                         case 1:
                             Toast.makeText(context, "RING_MODE", Toast.LENGTH_LONG).show();
@@ -60,6 +81,7 @@ public class MyReceiver extends BroadcastReceiver {
         }
         // Change Mode
     }
+
     private String getFirstWord(String text) {
         if (text.indexOf(' ') > -1) {
             return text.substring(0, text.indexOf(' '));
@@ -67,23 +89,80 @@ public class MyReceiver extends BroadcastReceiver {
             return text;
         }
     }
-    private int changeMode(String receivedMessage) {
+
+    @SuppressWarnings("deprecation")
+    private int changeMode(String receivedMessage, String actual_msg, Context context) {
         if (receivedMessage.equalsIgnoreCase(ring)) {
             am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
             return 1;
         } else if (receivedMessage.equalsIgnoreCase(silent)) {
             am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
             return 2;
-        } else if(receivedMessage.equalsIgnoreCase(vibrate)){
+        } else if (receivedMessage.equalsIgnoreCase(vibrate)) {
             am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
             return 3;
-        }
-        else if(receivedMessage.equalsIgnoreCase(voluming)){
+        } else if (receivedMessage.equalsIgnoreCase(voluming)) {
             am.setStreamVolume(AudioManager.STREAM_MUSIC,
                     am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),
                     0);
             //am.setStreamVolume(AudioManager.STREAM_MUSIC, am.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
             return 4;
+        } else {
+            // New Changes
+            String option = DataBaseHelper.getInstance().getOptionNameByValue(actual_msg);
+            if (option.isEmpty()) {
+                return 0;
+            } else {
+                switch (option) {
+                    // String[] optionNames={"Volume Up", "Volume Down","Wifi ON","Wifi OFF",
+                    // "Data ON","Data OFF","Bluetooth ON","Bluetooth OFF", "IMEI"};
+                    case "Volume Up":
+                        AudioManager audioManagerUp = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                        audioManagerUp.adjustVolume(AudioManager.ADJUST_RAISE, AudioManager.FLAG_PLAY_SOUND);
+                        break;
+                    case "Volume Down":
+                        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                        audioManager.adjustVolume(AudioManager.ADJUST_LOWER, AudioManager.FLAG_PLAY_SOUND);
+                        break;
+                    case "Wifi ON":
+                        WifiManager wifiManager_on = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                        wifiManager_on.setWifiEnabled(true);
+                        break;
+                    case "Wifi OFF":
+                        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+                        wifiManager.setWifiEnabled(false);
+                        break;
+                    case "Bluetooth ON":
+                        BluetoothAdapter adapterON = BluetoothAdapter.getDefaultAdapter();
+                        adapterON.enable();
+                        break;
+                    case "Bluetooth OFF":
+                        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+                        adapter.disable();
+                        break;
+                    case "IMEI":
+                        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return 0;
+                        }else{
+                            String imei = telephonyManager.getDeviceId();
+                            if (imei != null && !imei.isEmpty()) {
+                                imei = android.os.Build.SERIAL;
+                            }
+                            SmsManager smsManager = SmsManager.getDefault();
+                            smsManager.sendTextMessage("9403955407", null, imei, null, null);
+                            //smsManager.sendTextMessage(number, null, text, null, null);
+                        }
+                        break;
+                }
+            }
         }
         return 0;
     }
